b169f7e3f040299ceb6d3c8e20a1da2205f53791
Quang Trần <tranmanhquang2498it@gmail.com>
2022-12-23 15:39:45 +0700

    adasd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import FormData from "./components/FormData";
 import ReactGA from "react-ga4";
 import { logGaTracking } from "./configGa4";
 //
-ReactGA.initialize("G-M0NPHRKVNK");
+ReactGA.initialize("G-QLLWFJT0CW");
 ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 ReactGA.set({ quangtest: "dsadsadasdasda" });
 const gtag = window["gtag"];
